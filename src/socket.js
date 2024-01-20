914eca9ae0a9db598660b7c7c382dcbb7c0c9adf
Jatin Sharma <112959846+JatSh1804@users.noreply.github.com>
2024-01-21 01:48:41 +0530

    socket path

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socketinit = async () => {
-    return io(process.env.SOCKET_LINK || "http://localhost:3002", {
+    return io(process.env.SOCKET_LINK || "/", {
         auth: {
             token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzIjoiMTIzNDU2IiwibmFtZSI6IkphdGluIiwiaWF0IjoxNTE2MjM5MDIyfQ.Afl2vu2OwaB0caqnGZSimFiCWji4k3bnJzhYZKge-2w'
         }
