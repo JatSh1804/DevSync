d1651c7fef4b8d4b943537b8080d52d01e002d66
jatin <jatin1804sharma@gmail.com>
2025-03-13 02:06:59 +0530

    Fixed the client request routes

--- environment.ts
+++ environment.ts
@@ -1,11 +1,15 @@
 // require('dotenv').config()
-let apiRoute = 'production';
-console.log(import.meta.env.VITE_NODE_ENV)
+let apiRoute = '';
+
+console.log('Environment:', import.meta.env.VITE_NODE_ENV);
+
 if (import.meta.env.VITE_NODE_ENV === 'development') {
-    apiRoute = 'http://localhost:3002'; // replace with your server's address
+    apiRoute = 'http://localhost:3002';
+    console.log('Using development API route:', apiRoute);
 } else if (import.meta.env.VITE_NODE_ENV === 'production') {
-    apiRoute = ''; // replace with your production API's address
-}
+    apiRoute = import.meta.env.VITE_API_ROUTE; // Fallback to production as default
+    console.log('Using production API route:', apiRoute);
+} 
 
 export { apiRoute };
 // Use `apiRoute` in your API calls
